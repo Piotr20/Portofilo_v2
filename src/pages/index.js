5d5f0eab9b3c94720263f25956e74b5099b00ca7
Piotr Pospiech <55554468+Piotr20@users.noreply.github.com>
2021-06-05 11:52:28 +0200

    asscroll implementation

--- src/pages/index.js
+++ src/pages/index.js
@@ -5,8 +5,7 @@ import Header from "../components/layout-elements/header";
 import Container from "../components/layout-elements/container";
 import HorizontalShowcase from "../components/horizontalShowcase";
 import HorizontalShowcaseMobile from "../components/horizontalShowcaseMobile";
-import LocomotiveScroll from "locomotive-scroll";
-import "../styles/locomotiveScroll.css";
+import ASScroll from "@ashthornton/asscroll";
 import { gsap } from "gsap";
 import { ScrollTrigger } from "gsap/ScrollTrigger";
 import PersonIntro from "../components/personIntro";
@@ -17,31 +16,15 @@ gsap.registerPlugin(ScrollTrigger);
 const IndexPage = () => {
   const scrollRef = useRef(null);
   useEffect(() => {
-    const locoScroll = new LocomotiveScroll({
-      el: scrollRef.current,
-      smooth: true,
+    const asscroll = new ASScroll({
+      containerElement: "[asscroll-container]",
+      disableRaf: true,
     });
 
-    // tell ScrollTrigger to use these proxy methods for the ".smooth-scroll" element since Locomotive Scroll is hijacking things
-    ScrollTrigger.scrollerProxy(".scrollContainer", {
-      scrollTop(value) {
-        return arguments.length
-          ? locoScroll.scrollTo(value, 0, 0)
-          : locoScroll.scroll.instance.scroll.y;
-      }, // we don't have to define a scrollLeft because we're only scrolling vertically.
-      getBoundingClientRect() {
-        return {
-          top: 0,
-          left: 0,
-          width: window.innerWidth,
-          height: window.innerHeight,
-        };
-      },
-      // LocomotiveScroll handles things completely differently on mobile devices - it doesn't even transform the container at all! So to get the correct behavior and avoid jitters, we should pin things with position: fixed on mobile. We sense it by checking to see if there's a transform applied to the container (the LocomotiveScroll-controlled element).
-      pinType: document.querySelector(".scrollContainer").style.transform
-        ? "transform"
-        : "fixed",
+    window.addEventListener("load", () => {
+      asscroll.enable();
     });
+
     gsap.to(".work-heading-1", {
       scrollTrigger: {
         trigger: ".showcase-container",
@@ -133,16 +116,6 @@ const IndexPage = () => {
       stagger: 0.05,
       duration: 0.2,
       ease: "power4.easeOut",
-    });
-
-    // each time the window updates, we should refresh ScrollTrigger and then update LocomotiveScroll.
-    ScrollTrigger.addEventListener("load", () => locoScroll.update());
-
-    // after everything is set up, refresh() ScrollTrigger and update LocomotiveScroll because padding may have been added for pinning, etc.
-    ScrollTrigger.refresh();
-    const scrollContainer = document.querySelector(".scrollContainer");
-    scrollContainer.addEventListener("mousemove", () => {
-      console.log(scrollContainer.getBoundingClientRect());
     });
   }, []);
   return (
